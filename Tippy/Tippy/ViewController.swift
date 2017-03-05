c64ac2220e2c7764b72c4f8024e7c509e91d6e96
Arish Labroo <arishlabroo@hotmail.com>
2017-03-04 23:20:14 -0800

    remove comment

--- Tippy/Tippy/ViewController.swift
+++ Tippy/Tippy/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Tippy
-//
-//  Created by Arish Labroo on 3/3/17.
-//  Copyright © 2017 Arish Labroo. All rights reserved.
-//
 
 import UIKit
 
@@ -20,12 +13,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func onTap(_ sender: Any) {
